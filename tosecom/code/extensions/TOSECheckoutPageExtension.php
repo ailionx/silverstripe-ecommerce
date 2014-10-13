72b2e7ad3a0c9fcde182fea187f2057e41e64fe4
Shawn <jallen0927@hotmail.com>
2014-10-13 23:47:48 +1300

    orderForm in order page controller

--- tosecom/code/extensions/TOSECheckoutPageExtension.php
+++ tosecom/code/extensions/TOSECheckoutPageExtension.php
@@ -6,76 +6,10 @@
  * and open the template in the editor.
  */
 
-class TOSECheckoutPageExtension extends DataExtension {
+class TOSECheckoutPageControllerExtension extends Extension {
     
-    private static $db = array();
+
     
-    private static $allowed_actions = array(
-        'orderForm'
-    );
-    
-    /*        'FirstName' => 'Varchar(100)',
-        'SurName' => 'Varchar(100)',
-        'Phone' => 'Varchar(100)',
-        'Email' => 'Varchar(100)',
-        'StreetNumber' => 'Int',
-        'StreetName' => 'Varchar(100)',
-        'Suburb' => 'Varchar(100)',
-        'City' => 'Varchar(100)',
-        'Region' => 'Varchar(100)',
-        'Country' => 'Varchar(100)',
-        'PostCode' => 'Int'*/
-    
-    public function orderForm() {
-        $memberAddress = TOSEAddress::getCurrentMemberAddress();
-        var_dump($memberAddress); die();
-        $fields = new FieldList();
-        $customerInfoFields = new CompositeField();
-        $customerInfoFields->push(new LiteralField('CustomerInfo', '<h3>Customer Information</h3>'));
-        $customerInfoFields->push(new TextField('CustomerName', 'Name'));
-        $customerInfoFields->push(new TextField('CustomerEmail', 'Email'));
-        $customerInfoFields->push(new TextField('CustomerPhone', 'Phone'));
-        $fields->push($customerInfoFields);
-        
-        $shippingFields = new CompositeField();
-        $shippingFields->push(new LiteralField('CustomerInfo', '<h3>Shipping Address</h3>'));
-        $shippingFields->push(new TextField('ShippingFirstName', 'FirstName'));
-        $shippingFields->push(new TextField('ShippingSurName', 'SurName'));
-        $shippingFields->push(new TextField('ShippingPhone', 'Phone'));
-        $shippingFields->push(new TextField('ShippingStreetNumber', 'StreetNumber'));
-        $shippingFields->push(new TextField('ShippingStreetName', 'StreetName'));
-        $shippingFields->push(new TextField('ShippingSuburb', 'Suburb'));
-        $shippingFields->push(new TextField('ShippingCity', 'City'));
-        $shippingFields->push(new TextField('ShippingRegion', 'Region'));
-        $shippingFields->push(new TextField('ShippingCountry', 'Country'));
-        $shippingFields->push(new TextField('ShippingPostCode', 'PostCode'));
-        $fields->push($shippingFields);
-        
-        $invioceFields = new CompositeField();
-        $invioceFields->push(new CheckboxField('needInvioce', 'Need Invioce?'));
-        $fields->push($invioceFields);
-        
-        $billingFields = new CompositeField();
-        $billingFields->push(new LiteralField('CustomerInfo', '<h3>Billing Address</h3>'));
-        $billingFields->push(new TextField('BillingFirstName', 'FirstName'));
-        $billingFields->push(new TextField('BillingSurName', 'SurName'));
-        $billingFields->push(new TextField('BillingPhone', 'Phone'));
-        $billingFields->push(new TextField('BillingStreetNumber', 'StreetNumber'));
-        $billingFields->push(new TextField('BillingStreetName', 'StreetName'));
-        $billingFields->push(new TextField('BillingSuburb', 'Suburb'));
-        $billingFields->push(new TextField('BillingCity', 'City'));
-        $billingFields->push(new TextField('BillingRegion', 'Region'));
-        $billingFields->push(new TextField('BillingCountry', 'Country'));
-        $billingFields->push(new TextField('BillingPostCode', 'PostCode'));
-        $fields->push($billingFields);
-                
-        $actions = new FieldList(
-                new FormAction('handleConfirmation', 'Next')
-                );
-        
-        $form = new Form($this->owner, 'order-form', $fields, $actions);
-        
-        return $form;
-    }
+
     
 }
--- tosecom/code/pages/TOSECheckoutPage.php
+++ tosecom/code/pages/TOSECheckoutPage.php
@@ -15,13 +15,11 @@ class TOSECheckoutPage_Controller extends TOSEPage_Controller {
     
     private static $allowed_actions = array(
         'cartEmpty',
-        'orderForm',
-        'handleConfirmation'
+        'orderForm'
     );
     
     private static $url_handlers = array(
-        'empty' => 'cartEmpty',
-        'handleConfirmation' => 'confirm'
+        'empty' => 'cartEmpty'
     );
 
 
@@ -37,17 +35,75 @@ class TOSECheckoutPage_Controller extends TOSEPage_Controller {
 //    public function doNext() {
 //        
 //    }
-
-    public function handleConfirmation(SS_HTTPRequest $request) {
+    /**
+     * Function is generate form for order page
+     * @return \Form
+     */
+    public function orderForm() {
+        $memberAddress = TOSEAddress::getCurrentMemberAddress();
+        $member = Member::currentUser();
+        $fields = new FieldList();
+        $customerInfoFields = new CompositeField();
+        $customerInfoFields->push(new LiteralField('CustomerInfo', '<h3>Customer Information</h3>'));
+        $customerInfoFields->push(new TextField('CustomerName', 'Name', $member->FirstName." ".$member->Surname));
+        $customerInfoFields->push(new EmailField('CustomerEmail', 'Email', $member->Email));
+        $customerInfoFields->push(new TextField('CustomerPhone', 'Phone', $member->Phone));
+        $fields->push($customerInfoFields);
+        
+        $shippingFields = new CompositeField();
+        $shippingFields->push(new LiteralField('ShippingInfo', '<h3>Shipping Address</h3>'));
+        $shippingFields->push(new TextField('ShippingFirstName', 'First Name', $member->FirstName));
+        $shippingFields->push(new TextField('ShippingSurName', 'SurName', $member->Surname));
+        $shippingFields->push(new TextField('ShippingPhone', 'Phone', $member->Phone));
+        $shippingFields->push(new NumericField('ShippingStreetNumber', 'Street Number', $memberAddress->StreetNumber));
+        $shippingFields->push(new TextField('ShippingStreetName', 'Street Name', $memberAddress->StreetName));
+        $shippingFields->push(new TextField('ShippingSuburb', 'Suburb', $memberAddress->Suburb));
+        $shippingFields->push(new TextField('ShippingCity', 'City', $memberAddress->City));
+        $shippingFields->push(new TextField('ShippingRegion', 'Region', $memberAddress->Region));
+        $shippingFields->push(new TextField('ShippingCountry', 'Country', $memberAddress->Country));
+        $shippingFields->push(new NumericField('ShippingPostCode', 'PostCode', $memberAddress->PostCode));
+        $fields->push($shippingFields);
+        
+        $invioceFields = new CompositeField();
+        $invioceFields->push(new CheckboxField('needInvioce', 'Need Invioce?'));
+        $fields->push($invioceFields);
+        
+        $billingFields = new CompositeField();
+        $billingFields->push(new LiteralField('BillingInfo', '<h3>Billing Address</h3>'));
+        $billingFields->push(new TextField('BillingFirstName', 'First Name', $member->FirstName));
+        $billingFields->push(new TextField('BillingSurName', 'SurName', $member->Surname));
+        $billingFields->push(new TextField('BillingPhone', 'Phone', $member->Phone));
+        $billingFields->push(new NumericField('BillingStreetNumber', 'Street Number', $memberAddress->StreetNumber));
+        $billingFields->push(new TextField('BillingStreetName', 'Street Name', $memberAddress->StreetName));
+        $billingFields->push(new TextField('BillingSuburb', 'Suburb', $memberAddress->Suburb));
+        $billingFields->push(new TextField('BillingCity', 'City', $memberAddress->City));
+        $billingFields->push(new TextField('BillingRegion', 'Region', $memberAddress->Region));
+        $billingFields->push(new TextField('BillingCountry', 'Country', $memberAddress->Country));
+        $billingFields->push(new NumericField('BillingPostCode', 'PostCode', $memberAddress->PostCode));
+        $fields->push($billingFields);
+                
+        $actions = new FieldList(
+                new FormAction('confirm', 'Next')
+                );
+        
+        $required = new RequiredFields(
+                'Name',
+                'Email',
+                'Phone'
+            );
+        
+        $form = new Form($this, 'orderForm', $fields, $actions, $required);
+        
+        return $form;
+    }
+    
+    public function confirm($data, $form) {
         $cart = TOSECart::get_current_cart();
         if ($cart->cartEmpty()) {
             return $this->redirect($this->Link()."cartEmpty");
         }
-        
-        $data = $request->postVars();
         var_dump($data); die();
     }
     
-
     
 }
